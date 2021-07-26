cec999bd15ebbc558836a6770159ae6c9e8c686c
Bijan Vakili <bijanvakili@users.noreply.github.com>
2021-07-25 21:21:09 -0700

    Remove gulpfile

--- gulpfile.ts
+++
@@ -1,8 +0,0 @@
-import * as gulp from "gulp";
-import * as ts from "gulp-typescript";
-
-const tsProject = ts.createProject("tsconfig.json");
-
-gulp.task("default", () => {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
-});
